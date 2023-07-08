d92a4871e4d51fb1e2fae1f014badeaeb93300fc
Timofei Jääger <tim.jaager@gmail.com>
2023-07-09 02:15:27 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+    const inputStr = core.getInput('string');
+    console.log(`Manipulating string: ${inputStr}`);
+    if(inputStr == "success"){
+        const result = '✅' + inputStr.charAt(0).toUpperCase() + inputStr.slice(1).toLowerCase();
+        console.log(`result: ${result}`);
+        core.setOutput("result", result);
+    }
+
+    if(inputStr == "failure"){
+        const result = '❌' + inputStr.charAt(0).toUpperCase() + inputStr.slice(1).toLowerCase();
+        console.log(`result: ${result}`);
+        core.setOutput("result", result);
+    }
+} catch (error) {
+    core.setFailed(error.message);
+}
